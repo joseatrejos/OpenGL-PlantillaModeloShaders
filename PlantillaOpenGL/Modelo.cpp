d09c87c0d72aca78ca84a630e0cebc6fd981d45a
cecy castañeda <ceciliacast00@hotmail.com>
2019-03-14 10:17:55 -0700

    Se terminó de implementar la clase Modelo para poder crear instancias de dicho objeto y crear figuras más fácilmente.

---
+++ PlantillaOpenGL/Modelo.cpp
@@ -0,0 +1,36 @@
+#include "stdafx.h"
+#include "Modelo.h"
+
+Modelo::Modelo() {
+
+}
+
+void Modelo::inicializarVertexArray
+(GLuint posicionID, GLuint colorID) {
+	// Creación del vertex array
+	glGenVertexArrays(1, &vertexArrayID);
+	glBindVertexArray(vertexArrayID);
+
+	// Creación del vertex buffer
+	glGenBuffers(1, &bufferID);
+	glBindBuffer(GL_ARRAY_BUFFER, bufferID);
+
+	// Llenado el buffer
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertice) * vertices.size(), vertices.data(), GL_STATIC_DRAW);
+
+	// Habilitar atributos de shader
+	glEnableVertexAttribArray(posicionID);
+	glEnableVertexAttribArray(colorID);
+
+	// Especificar a OpenGL como se va a comunicar
+	glVertexAttribPointer(posicionID, 3, GL_FLOAT, GL_FALSE, sizeof(Vertice), 0);
+	glVertexAttribPointer(colorID, 4, GL_FLOAT, GL_FALSE, sizeof(Vertice), (void*)sizeof(vec3));
+}
+
+void Modelo::dibujar(GLenum modoDibujo) {
+	shader->enlazar();
+		glBindVertexArray(vertexArrayID);
+			glDrawArrays(modoDibujo, 0, vertices.size());
+		glBindVertexArray(0);
+	shader->desenlazar();
+}
